e5ddf1697d1e30c4819e2dd918a030f92cd49f65
YAlmonkari <36625605+YAlmonkari@users.noreply.github.com>
2018-02-23 13:36:35 +0100

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -35,17 +35,3 @@ function czas(czas) {
 }
 // Get the video
 var video = document.getElementById("myVideo");
-
-// Get the button
-var btn = document.getElementById("myBtn");
-
-// Pause and play the video, and change the button text
-function myFunction() {
-    if (video.paused) {
-        video.play();
-        btn.innerHTML = "Pause";
-    } else {
-        video.pause();
-        btn.innerHTML = "Play";
-    }
-}
