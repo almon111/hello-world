31f022a1d6853bf06278e412bdf4b44587aa06ff
YAlmonkari <36625605+YAlmonkari@users.noreply.github.com>
2018-03-09 11:32:11 +0100

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -3,41 +3,4 @@ function kolor(kolor)
 {
 document.bgColor=kolor;
 }
-function move() {
-    var elem = document.getElementById("myBar");
-    var width = 10;
-    var id = setInterval(frame, 10);
-    function frame() {
-        if (width >= 100) {
-            clearInterval(id);
-        } else {
-            width++;
-            elem.style.width = width + '%';
-            elem.innerHTML = width * 1 + '%';
-        }
-    }
-} 
-function czas(czas) {
- 
- 
-    var date = new Date();
-    var sekundy = date.getSeconds(),
-        minuty = date.getMinutes(),
-        godziny = date.getHours();
- 
- 
-    if (czas == 'am' && godziny >= 12) godziny -= 12;
- 
- 
-    document.getElementById('czas').innerHTML = 'godzina : ' + godziny + ' : ' + minuty + ' : ' + sekundy;
- 
- 
-}
-// Get the video
-var video = document.getElementById("myVideo");
-var app6 = new Vue({
-  el: '#app-6',
-  data: {
-    message: 'Hello Vue!'
-  }
-});
+
